ccbd7da34a99f002c8d672ee3740e5e1c69490f7
Jose Fons <josefoons@gmail.com>
2020-04-07 14:10:22 +0200

    Testing cambio de pantalla

--- app/src/main/java/es/josefons/reactown/ui/main/MainFragment.java
+++ app/src/main/java/es/josefons/reactown/ui/main/MainFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import es.josefons.reactown.R;
 
@@ -34,6 +36,15 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
+
+        Button nose = getView().findViewById(R.id.boton);
+        nose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.mainToRegistro);
+                //MainFragmentDirections.mainToRegistro()
+            }
+        });
     }
 
 }
